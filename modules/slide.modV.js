fa7f2c03652dcc4c85a0729e1cddbe0b711b1452
Sam Wray <hello@brkbrkbrk.com>
2014-12-19 01:11:41 +0000

    Revert "Adds both axis."
    This reverts commit f4a3f67fcb9686dadbe0ee6cf6269ee79b1a8675.

--- modules/slide.modV.js
+++ modules/slide.modV.js
@@ -4,8 +4,7 @@ var slide = function() {
 		author: '2xAA',
 		version: 0.1,
 		controls: [
-			 {type: 'range', variable: 'speedX', min: 0, max: 100, label: 'Speed X', varType: 'int'}
-			 {type: 'range', variable: 'speedY', min: 0, max: 100, label: 'Speed Y', varType: 'int'}
+			 {type: 'range', variable: 'speed', min: 0, max: 100, label: 'Speed', varType: 'int'}
 		]
 	};
 
@@ -14,34 +13,26 @@ var slide = function() {
 		newCanvas2.height = canvas.height;
 	};
 
-	this.speedY = 0;
-	var offsetX = 0;
-	this.speedY = 0;
-	var offsetY = 0;
+	this.speed = 0;
+	var offset = 0;
 	var base = 0;
 
 	var newCanvas2 = document.createElement('canvas');
 	var newCtx2 = newCanvas2.getContext("2d");
 
 	this.draw = function(canvas, ctx, amplitudeArray) {
-		if(offsetY >= canvas.height) {
-			offsetY = (offsetX-canvas.height)+this.speedY;
+		if(offset >= canvas.height) {
+			offset = (offset-canvas.height)+this.speed;
 		} else {
-			offsetY += this.speedY;
-		}
-		
-		if(offsetX >= canvas.width) {
-			offsetX = (offsetY-canvas.width)+this.speedX;
-		} else {
-			offsetX += this.speedX;
+			offset += this.speed;
 		}
 
 		ctx.save();
 		newCtx2.clearRect(0, 0, newCanvas2.width, newCanvas2.height);
 		newCtx2.drawImage(canvas, 0, 0, canvas.width, canvas.height, 0, 0, canvas.width, canvas.height);
 		
-		ctx.drawImage(newCanvas2, offsetX, offsetY);
-		ctx.drawImage(newCanvas2, (-canvas.width + offsetX), (-canvas.height + offsetY));
+		ctx.drawImage(newCanvas2, 0, offset);
+		ctx.drawImage(newCanvas2, 0, (-canvas.height + offset));
 		
 		
 		ctx.restore();
