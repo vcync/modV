35f5f0bbc46c707846eed0b8589bcfea7baf9d07
Sam Wray <hello@brkbrkbrk.com>
2016-03-14 22:43:50 +0000

    Adds the ability to remove modules from the modOrder array. Removes redundant code

--- src/modV-setModOrder.js
+++ src/modV-setModOrder.js
@@ -5,9 +5,20 @@
 	modV.prototype.setModOrder = function(modName, order) {
 		var self = this;
 
+		if(order < 0) {
+			console.log('attempting splice', this.modOrder);
+			var index = -1;
+			this.modOrder.forEach(function(mod, idx) {
+				if(modName === mod) index = idx;
+			});
+			if(index > -1) this.modOrder.splice(index, 1);
+			console.log('index is:', index, this.modOrder);
+			return false;
+		}
+
 		if(this.modOrder[order] === 'undefined') this.modOrder[order] = modName;
 		else {
-			var index = -1;
+			var index = -1; // jshint ignore:line
 			this.modOrder.forEach(function(mod, idx) {
 				if(modName === mod) index = idx;
 			});
@@ -18,27 +29,6 @@
 				self.registeredMods[mod].info.order = idx;
 			});
 		}
-		/*
-		// Reorder DOM elements (ugh ;_;)
-		var list = this.controllerWindow.document.body;
-		var items = list.querySelectorAll('fieldset[data-name]');
-		var itemsArr = [];
-		for (var i in items) {
-			if(items[i].nodeType === 1) { // Remove the whitespace text nodes
-				itemsArr.push(items[i]);
-			}
-		}
-		
-		itemsArr.sort(function(a, b) {
-			var aOrder = this.registeredMods[a.dataset.name].info.order;
-			var bOrder = this.registeredMods[b.dataset.name].info.order;
-			
-			return aOrder-bOrder;
-		});
-		
-		for(i = 0; i < itemsArr.length; ++i) {
-			list.appendChild(itemsArr[i]);
-		}*/
 		
 		return order;
 	};
