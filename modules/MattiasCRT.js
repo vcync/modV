1333d9afdaafe38a05ef44707f459a9453f6f7bc
Sam Wray <hello@brkbrkbrk.com>
2016-03-14 22:03:43 +0000

    Removes copied controls

--- modules/MattiasCRT.js
+++ modules/MattiasCRT.js
@@ -23,34 +23,4 @@ var MattiasCRT = new modVC.ModuleShader({
 	shaderFile: "/MattiasCRT/shader.html" // path to HTML file within modules directory with shader script tags
 });
 
-/*Bulge.add(new modVC.RangeControl({
-	variable: 'amount',
-	label: 'Bulge Size',
-	varType: 'float',
-	min: 0.0,
-	max: 1.14,
-	step: 0.001,
-	default: 0.5
-}));
-
-Bulge.add(new modVC.RangeControl({
-	variable: 'centerX',
-	label: 'Center X',
-	varType: 'float',
-	min: 0.0,
-	max: 1.0,
-	step: 0.001,
-	default: 0.5
-}));
-
-Bulge.add(new modVC.RangeControl({
-	variable: 'centerY',
-	label: 'Center Y',
-	varType: 'float',
-	min: 0.0,
-	max: 1.0,
-	step: 0.001,
-	default: 0.5
-}));*/
-
 modVC.register(MattiasCRT);
