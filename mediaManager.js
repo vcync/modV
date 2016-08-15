5e36914fab1f49c88768f7417da9c2557b1cef79
2xAA <hello@brkbrkbrk.com>
2016-08-15 04:04:05 +0100

    Improves mediaManager with watch task

--- mediaManager.js
+++ mediaManager.js
@@ -1,11 +1,13 @@
 //jshint node:true
+
 var restify = require('restify'),
 	dive = require('dive'),
 	ws = require('watershed'),
 	fs = require('fs'),
 	path = require('path'),
 	animated = require('animated-gif-detector'),
-	ffmpeg = require('fluent-ffmpeg');
+	ffmpeg = require('fluent-ffmpeg'),
+	watch = require('node-watch');
 
 ws = new ws.Watershed();
 
@@ -40,60 +42,70 @@ function getDirectories(srcpath) {
 
 var directories = getDirectories(path.resolve('./media'));
 
-directories.forEach(function(dir) {
+function createDirectories(callback) {
+	directories.forEach(function(dir) {
 
-	if(!(dir in profiles)) { //TODO: redundant if statement??
-		profiles[dir] = {
-			presets: {},
-			palettes:{},
-			files: {
-				images: [],
-				videos: []
+		if(!(dir in profiles)) { //TODO: redundant if statement??
+			profiles[dir] = {
+				presets: {},
+				palettes:{},
+				files: {
+					images: [],
+					videos: []
+				}
+			}; // Create new profile
+			console.log('New profile created: ', dir);
+		} else {
+			// This should never error
+			// If it does, we're screwed
+			throw 'Somehow two directories with the same name in the same directory were discovered.\nPlease check this and try again.';
+		}
+
+		// Create folders that need to exist
+		/* According to the NodeJS docs, this is the recommended way
+		   to check if a directory exists (i.e. just go for it and
+	   	catch the errors) */
+
+		fs.open(cwd + '/media/' + dir + '/image', 'r', function(err) {
+			if(err && err.code === 'ENOENT') {
+				fs.mkdir(cwd + '/media/' + dir + '/image');
 			}
-		}; // Create new profile
-		console.log('New profile created: ', dir);
-	} else {
-		// This should never error
-		// If it does, we're screwed
-		throw 'Somehow two directories with the same name in the same directory were discovered.\nPlease check this and try again.';
-	}
+		});
 
-	// Create folders that need to exist
-	/* According to the NodeJS docs, this is the recommended way
-	   to check if a directory exists (i.e. just go for it and
-   	catch the errors) */
+		fs.open(cwd + '/media/' + dir + '/video', 'r', function(err) {
+			if(err && err.code === 'ENOENT') {
+				fs.mkdir(cwd + '/media/' + dir + '/video');
+			}
+		});
 
-	fs.open(cwd + '/media/' + dir + '/image', 'r', function(err, fd) {
-		if(err && err.code == 'ENOENT') {
-			fs.mkdir(cwd + '/media/' + dir + '/image');
-		}
+		fs.open(cwd + '/media/' + dir + '/preset', 'r', function(err) {
+			if(err && err.code === 'ENOENT') {
+				fs.mkdir(cwd + '/media/' + dir + '/preset');
+			}
+		});
+
+		fs.open(cwd + '/media/' + dir + '/palette', 'r', function(err) {
+			if(err && err.code === 'ENOENT') {
+				fs.mkdir(cwd + '/media/' + dir + '/palette');
+			}
+		});
+
 	});
-
-	fs.open(cwd + '/media/' + dir + '/video', 'r', function(err, fd) {
-		if(err && err.code == 'ENOENT') {
-			fs.mkdir(cwd + '/media/' + dir + '/video');
-		}
-	});
-
-	fs.open(cwd + '/media/' + dir + '/preset', 'r', function(err, fd) {
-		if(err && err.code == 'ENOENT') {
-			fs.mkdir(cwd + '/media/' + dir + '/preset');
-		}
-	});
-
-	fs.open(cwd + '/media/' + dir + '/palette', 'r', function(err, fd) {
-		if(err && err.code == 'ENOENT') {
-			fs.mkdir(cwd + '/media/' + dir + '/palette');
-		}
-	});
-
-});
+	callback();
+}
 
 /* Server */
 var server = restify.createServer({
 	handleUpgrades: true,
 	name: 'modV profile manager'
 });
+
+var clients = [];
+
+function update(shed) {
+	console.log('\nSending client profiles data');
+	shed.send(JSON.stringify({type: 'update', payload: profiles}));
+}
 
 server.get('/', function upgradeRoute(req, res, next) {
 	if (!res.claimUpgrade) {
@@ -106,11 +118,9 @@ server.get('/', function upgradeRoute(req, res, next) {
 	var upgrade = res.claimUpgrade();
 
 	var shed = ws.accept(req, upgrade.socket, upgrade.head);
+	clients.push(shed);
 
-	function update() {
-		console.log('\nSending client profiles data');
-		shed.send(JSON.stringify({type: 'update', payload: profiles}));
-	}
+	update(shed);
 
 	shed.on('text', function(msg) {
 		var parsed = JSON.parse(msg);
@@ -121,7 +131,7 @@ server.get('/', function upgradeRoute(req, res, next) {
 			switch(parsed.request) {
 				case 'update':
 					
-					update();
+					update(shed);
 
 				break;
 
@@ -178,89 +188,110 @@ server.get('/', function upgradeRoute(req, res, next) {
 var mediaDir = cwd + pathSeparator + 'media';
 
 /* Slosh through media dir */
-dive(mediaDir, { all: false }, function(err, file) {
+function mediaSearch(callback) {
+	dive(mediaDir, { all: false }, function(err, file) {
 
-	if(err) throw err;
-	//console.log(file, file.split('.').pop());
-	var pathReplaced = file.replace(mediaDir, '');
-	pathReplaced = pathReplaced.split(pathSeparator);
+		if(err) throw err;
+		//console.log(file, file.split('.').pop());
+		var pathReplaced = file.replace(mediaDir, '');
+		pathReplaced = pathReplaced.split(pathSeparator);
 
-	var dirSplit = file.split(pathSeparator);
+		var dirSplit = file.split(pathSeparator);
 
-	var profile 	= pathReplaced[1];
-	var directory 	= pathReplaced[2];
-	var filename 	= dirSplit[dirSplit.length-1].split('.')[0];
-	var fileExt		= file.split('.').pop();
-	var filePath	= file.replace(cwd, '.');
-	var path 		= filePath.replace(filename + '.' + fileExt, '');
-	var fileParsed;
+		var profile 	= pathReplaced[1];
+		var directory 	= pathReplaced[2];
+		var filename 	= dirSplit[dirSplit.length-1].split('.')[0];
+		var fileExt		= file.split('.').pop();
+		var filePath	= file.replace(cwd, '.');
+		var fileParsed;
 
 
-	if(directory == 'palette') {
-		fileParsed = JSON.parse(fs.readFileSync(file, 'utf8')); // sync because we don't want to finish before reading has occurred
-		profiles[profile].palettes[filename] = fileParsed;
-		console.log('🎨  Found palette in', profile);
-	}
+		if(directory === 'palette') {
+			fileParsed = JSON.parse(fs.readFileSync(file, 'utf8')); // sync because we don't want to finish before reading has occurred
+			profiles[profile].palettes[filename] = fileParsed;
+			console.log('🎨  Found palette in', profile);
+		}
 
-	if(directory == 'preset') {
-		fileParsed = JSON.parse(fs.readFileSync(file, 'utf8')); // sync because we don't want to finish before reading has occurred
-		profiles[profile].presets[filename] = fileParsed;
-		console.log('💾  Found preset data in', profile);
-	}
+		if(directory === 'preset') {
+			fileParsed = JSON.parse(fs.readFileSync(file, 'utf8')); // sync because we don't want to finish before reading has occurred
+			profiles[profile].presets[filename] = fileParsed;
+			console.log('💾  Found preset data in', profile);
+		}
 
-	console.log(profile, profiles, profiles[profile]);
+		console.log(profile, profiles, profiles[profile]);
 
-	if(fileExt.toLowerCase() in viableVideo) {
+		if(fileExt.toLowerCase() in viableVideo) {
 
-		profiles[profile].files.videos.push({'name': filename, 'path': filePath});
-		console.log('📼  Found video in', profile);
-	}
+			profiles[profile].files.videos.push({'name': filename, 'path': filePath});
+			console.log('📼  Found video in', profile);
+		}
 
-	if(fileExt.toLowerCase() in viableImage) {
-		console.log('📷  Found image in', profile);
+		if(fileExt.toLowerCase() in viableImage) {
+			console.log('📷  Found image in', profile);
 
-		if(fileExt.toLowerCase() === 'gif' && animated(fs.readFileSync(filePath))) {
+			if(fileExt.toLowerCase() === 'gif' && animated(fs.readFileSync(filePath))) {
 
-			console.log('Animated GIF detected:', filePath);
-			var outputFile = cwd + '/media/' + profile + '/video/' + filename + '.mp4';
+				console.log('Animated GIF detected:', filePath);
+				var outputFile = cwd + '/media/' + profile + '/video/' + filename + '.mp4';
 
-			// Check if we need to convert
-			fs.open(outputFile, 'r', function(err, fd) {
-				if(err && err.code == 'ENOENT') {
+				// Check if we need to convert
+				fs.open(outputFile, 'r', function(err) {
+					if(err && err.code === 'ENOENT') {
+						
+						console.log('Converting', filePath, 'to MP4');
+
+						ffmpeg(filePath)
+							.inputFormat('gif')
+							.format('mp4')
+							.noAudio()
+							.videoCodec('libx264')
+							.on('error', function(err) {
+								console.log('An error occurred converting ' + filePath + ':', err.message);
+							})
+							.on('end', function() {
+								console.log('Processing', filePath, 'finished!');
+								profiles[profile].files.videos.push({'name': filename, 'path': outputFile.replace(cwd, '.')});
+							})
+							.save(outputFile);
+					}
+				});
+
+			} else {
+				profiles[profile].files.images.push({'name': filename, 'path': filePath});
+			}
+			
+		}
+
+		//profiles[profile]
+
+	}, function() {
+		console.log('\n👍  Finished sloshing through media, here\'s what I got: \n');
+		console.log(require('util').inspect(profiles, true, 10));
+		callback();
+	});
+}
+
+createDirectories(function() {
+	mediaSearch(function() {
+		server.listen(3132, function() {
+			console.log('\n_%s listening at %s_', server.name, server.url);
+
+			console.log('Watching ./media/ for changes...');
+			watch('./media/', { recursive: true, followSymLinks: true }, function(filepath) {
+
+				if(path.parse(filepath).base !== '.DS_Store') {
+					console.log(filepath, ' changed - updating media and seding to clients');
 					
-					console.log('Converting', filePath, 'to MP4');
-
-					var command = ffmpeg(filePath)
-						.inputFormat('gif')
-						.format('mp4')
-						.noAudio()
-						.videoCodec('libx264')
-						.on('error', function(err) {
-							console.log('An error occurred converting ' + filePath + ':', err.message);
-						})
-						.on('end', function() {
-							console.log('Processing', filePath, 'finished!');
-							profiles[profile].files.videos.push({'name': filename, 'path': outputFile.replace(cwd, '.')});
-						})
-						.save(outputFile);
+					profiles = {};
+					createDirectories(function() {
+						mediaSearch(function() {
+							clients.forEach(update);
+						});
+					});
 				}
+
 			});
 
-		} else {
-			profiles[profile].files.images.push({'name': filename, 'path': filePath});
-		}
-		
-	}
-
-	//profiles[profile]
-
-}, function() {
-
-	console.log('\n👍  Finished sloshing through media, here\'s what I got: \n');
-	console.log(require('util').inspect(profiles, true, 10));
-
-	server.listen(3132, function() {
-		console.log('\n_%s listening at %s_', server.name, server.url);
+		});
 	});
-
 });
