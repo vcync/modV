c445898b11bf21dde2fe6d71a82e9a390a016a8c
Sam Wray <sam@wray.pro>
2017-02-08 12:58:52 +0000

    Adds new shader modules

---
+++ modules/Brightness-Contrast.js
@@ -0,0 +1,46 @@
+class BrightCont extends modV.ModuleShader {
+	constructor() {
+		super({
+			info: {
+				name: 'Brightess Contrast',
+				author: '2xAA',
+				version: 0.1,
+				meyda: [], // returned variables passed to the shader individually as uniforms
+				controls: [], // variabled passed to the shader individually as uniforms
+				uniforms: {
+					brightness: {
+						type: 'f',
+						value: 0.0
+					},
+					contrast: {
+						type: 'f',
+						value: 1.0
+					}
+				} // Three.JS style uniforms
+			},
+			shaderFile: "/Brightness-Contrast/shader.html" // path to HTML file within modules directory with shader script tags
+		});
+				
+		this.add(new modV.RangeControl({
+			variable: 'brightness',
+			label: 'Brightness',
+			varType: 'float',
+			min: -1.0,
+			max: 1.0,
+			step: 0.001,
+			default: 0.0
+		}));
+
+		this.add(new modV.RangeControl({
+			variable: 'contrast',
+			label: 'Contrast',
+			varType: 'float',
+			min: 0.0,
+			max: 2.0,
+			step: 0.001,
+			default: 1.0
+		}));
+	}
+}
+
+modV.register(BrightCont);
