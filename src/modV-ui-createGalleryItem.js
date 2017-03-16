483ec692cf628dd9def6e943cb2ba5529142cabf
Sam Wray <sam@wray.pro>
2017-03-16 13:16:03 +0000

    Removes redundant code and comments, updates loop to use bind

--- src/modV-ui-createGalleryItem.js
+++ src/modV-ui-createGalleryItem.js
@@ -1,29 +1,24 @@
 module.exports = function(modV) {
 
 	modV.prototype.createGalleryItem = function(oldModule) {
-		var self = this;
 
 		if(
-			!(oldModule instanceof self.Module2D) &&
-			!(oldModule instanceof self.ModuleShader) &&
-			!(oldModule instanceof self.Module3D) &&
-			!(oldModule instanceof self.ModuleScript)
+			!(oldModule instanceof this.Module2D) &&
+			!(oldModule instanceof this.ModuleShader) &&
+			!(oldModule instanceof this.Module3D) &&
+			!(oldModule instanceof this.ModuleScript)
 		) return;
 
-		if(self.headless) return;		
+		let template = this.templates.querySelector('#gallery-item');
+		let galleryItem = document.importNode(template.content, true);
 
-		self.galleryModules = [];
-		
-		var template = self.templates.querySelector('#gallery-item');
-		var galleryItem = document.importNode(template.content, true);
-
-		var previewCanvas = galleryItem.querySelector('canvas');
+		let previewCanvas = galleryItem.querySelector('canvas');
 
 		// Module variables
-		var previewCtx = previewCanvas.getContext('2d');
-		
+		let previewCtx = previewCanvas.getContext('2d');
+
 		// Clone module
-		var Module = self.createModule(oldModule, previewCanvas, previewCtx, true);
+		let Module = this.createModule(oldModule, previewCanvas, previewCtx, true);
 
 		// Setup any preview settings for gallery item
 		if('previewValues' in Module.info) {
@@ -31,7 +26,7 @@ module.exports = function(modV) {
 				Module[key] = value;
 			});
 		}
-		
+
 		document.querySelector('.gallery').appendChild(galleryItem);
 
 		// Tell the Module it is in the gallery
@@ -39,170 +34,111 @@ module.exports = function(modV) {
 
 		// Pull back initialised node from DOM
 		galleryItem = document.querySelector('.gallery .gallery-item:last-child');
-		var galleryItemTitle = galleryItem.querySelector('span.title');
+		let galleryItemTitle = galleryItem.querySelector('span.title');
 		galleryItemTitle.textContent = Module.info.name;
 
 		// Set data
-		// TODO: make sure this follows the HTML5 attributes spec: https://html.spec.whatwg.org/multipage/syntax.html#attributes-2
 		galleryItem.dataset.moduleName = Module.info.name.replace(' ', '-');
 
-		// Draw preview
-		//giMouseEnter(Module, previewCanvas, previewCtx, self);
-
-		// Draw name
-		giMouseOut(Module, previewCanvas, previewCtx, self);
-
-		previewCanvas.addEventListener('mouseenter', function() {
-			/*var loop = giMouseEnter(Module, previewCanvas, previewCtx, self);
-			interval = setInterval(loop, 1000/60);*/
+		previewCanvas.addEventListener('mouseenter', () => {
 			mouseOver = true;
-			activeVariables = [Module, previewCanvas, previewCtx, self];
-			raf = requestAnimationFrame(loop);
+			activeVariables = [Module, previewCanvas, previewCtx];
+			raf = requestAnimationFrame(loop.bind(this));
 		});
 
 		previewCanvas.addEventListener('mouseout', function() {
-			giMouseOut(Module, previewCanvas, previewCtx, self);
 			cancelAnimationFrame(raf);
 			mouseOver = false;
 			activeVariables = [];
 		});
 
-/*		previewCanvas.addEventListener('mousemove', function(evt) {
-			mousePos = getMousePos(previewCanvas, evt, true);
-	   	}, false);*/
-
 	};
 
-	//var mousePos = {x: 0, y: 0};
-	var mouseOver = false;
-	var raf = null;
-	var activeVariables = [];
+	let mouseOver = false;
+	let raf = null;
+	let activeVariables = [];
 
-	//function giMouseEnter(Module, canvas, ctx, self) {
+	function loop(delta) {
 
-		function loop(delta) {
-			
-			if(mouseOver) raf = requestAnimationFrame(loop);
-			else cancelAnimationFrame(raf);
+		if(mouseOver) raf = requestAnimationFrame(loop.bind(this));
+		else cancelAnimationFrame(raf);
 
-			if(activeVariables.length !== 4) {
-				return;
-			}
+		let Module = activeVariables[0],
+			canvas = activeVariables[1],
+			ctx = activeVariables[2];
 
-			var Module = activeVariables[0],
-				canvas = activeVariables[1],
-				ctx = activeVariables[2],
-				self = activeVariables[3];
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-			//ctx.clearRect(0, 0, mousePos.x, canvas.height);
-			//ctx.clearRect(0, 0, Math.round(canvas.width/2), canvas.height);
-			ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-			//var positionInfo = canvas.getBoundingClientRect();
-
-			//var largeWidth = Math.round(Math.map(mousePos.x, 0, positionInfo.width, 0, self.canvas.width));
-
-			if(Module.info.previewWithOutput || Module instanceof self.ModuleShader) {
-				ctx.drawImage(self.outputCanvas, 0, 0, canvas.width, canvas.height);
-			}
-
-			//ctx.drawImage(self.canvas, largeWidth, 0, self.canvas.width, self.canvas.height, mousePos.x, 0, canvas.width, canvas.height);
-			//ctx.drawImage(self.canvas, Math.round(self.canvas.width/2), 0, self.canvas.width, self.canvas.height, Math.round(canvas.width/2), 0, canvas.width, canvas.height);
-			
-
-			if(Module instanceof self.ModuleShader) {
-
-				var _gl = self.shaderEnv.gl;
-
-				// Switch program
-				if(Module.programIndex !== self.shaderEnv.activeProgram) {
-
-					self.shaderEnv.activeProgram = Module.programIndex;
-
-					_gl.useProgram(self.shaderEnv.programs[Module.programIndex]);
-				}
-
-				// Copy Main Canvas to Shader Canvas 
-				self.shaderEnv.texture = _gl.texImage2D(
-					self.shaderEnv.gl.TEXTURE_2D,
-					0,
-					_gl.RGBA,
-					_gl.RGBA,
-					_gl.UNSIGNED_BYTE,
-					self.outputCanvas
-				);
-
-				// Set Uniforms
-				if('uniforms' in Module.info) {
-					forIn(Module.info.uniforms, (uniformKey, uniform) => {
-						var uniLoc = _gl.getUniformLocation(self.shaderEnv.programs[self.shaderEnv.activeProgram], uniformKey);
-						var value;
-
-						switch(uniform.type) {
-							case 'f':
-								value = parseFloat(Module[uniformKey]);
-								_gl.uniform1f(uniLoc, value);
-								break;
-
-							case 'i':
-								value = parseInt(Module[uniformKey]);
-								_gl.uniform1i(uniLoc, value);
-								break;
-
-							case 'b':
-								value = Module[uniformKey];
-								if(value) value = 1;
-								else value = 0;
-								
-								_gl.uniform1i(uniLoc, value);
-								break;
-
-						}
-					});
-				}
-
-				// Render
-				self.shaderEnv.render(delta, canvas);
-
-				// Copy Shader Canvas to Main Canvas
-				ctx.drawImage(
-					self.shaderEnv.canvas,
-					0,
-					0,
-					canvas.width,
-					canvas.height
-				);
-
-			} else if('draw' in Module) {
-				ctx.save();
-				Module.draw(canvas, ctx, self.videoStream, self.myFeatures, self.meyda, delta, self.bpm, self.kick);
-				ctx.restore();
-			}
+		if(Module.info.previewWithOutput || Module instanceof this.ModuleShader) {
+			ctx.drawImage(this.outputCanvas, 0, 0, canvas.width, canvas.height);
 		}
 
-	//}
+		if(Module instanceof this.ModuleShader) {
 
-	function giMouseOut(Module, canvas, ctx) {
-		//ctx.fillStyle = 'rgba(0,0,0,0.5)';
-		//ctx.fillRect(0, 0, canvas.width, canvas.height);
-		//ctx.fillStyle = 'white';
-		//var textWidth = ctx.measureText(Module.info.name).width;
-		//ctx.fillText(Module.info.name, canvas.width/2 - textWidth/2, canvas.height/2);
+			let _gl = this.shaderEnv.gl;
+
+			// Switch program
+			if(Module.programIndex !== this.shaderEnv.activeProgram) {
+
+				this.shaderEnv.activeProgram = Module.programIndex;
+
+				_gl.useProgram(this.shaderEnv.programs[Module.programIndex]);
+			}
+
+			// Copy Main Canvas to Shader Canvas
+			this.shaderEnv.texture = _gl.texImage2D(
+				this.shaderEnv.gl.TEXTURE_2D,
+				0,
+				_gl.RGBA,
+				_gl.RGBA,
+				_gl.UNSIGNED_BYTE,
+				this.outputCanvas
+			);
+
+			// Set Uniforms
+			if('uniforms' in Module.info) {
+				forIn(Module.info.uniforms, (uniformKey, uniform) => {
+					let uniLoc = _gl.getUniformLocation(this.shaderEnv.programs[this.shaderEnv.activeProgram], uniformKey);
+					let value;
+
+					switch(uniform.type) {
+						case 'f':
+							value = parseFloat(Module[uniformKey]);
+							_gl.uniform1f(uniLoc, value);
+							break;
+
+						case 'i':
+							value = parseInt(Module[uniformKey]);
+							_gl.uniform1i(uniLoc, value);
+							break;
+
+						case 'b':
+							value = Module[uniformKey];
+							if(value) value = 1;
+							else value = 0;
+
+							_gl.uniform1i(uniLoc, value);
+							break;
+					}
+				});
+			}
+
+			// Render
+			this.shaderEnv.render(delta, canvas);
+
+			// Copy Shader Canvas to Main Canvas
+			ctx.drawImage(
+				this.shaderEnv.canvas,
+				0,
+				0,
+				canvas.width,
+				canvas.height
+			);
+
+		} else if('draw' in Module) {
+			ctx.save();
+			Module.draw(canvas, ctx, this.videoStream, this.activeFeatures, this.meyda, delta, this.bpm, this.kick);
+			ctx.restore();
+		}
 	}
-
-/*	function getMousePos(canvas, evt, round) {
-		var rect = canvas.getBoundingClientRect();
-		if(round) {
-			return {
-				x: Math.floor(evt.clientX - rect.left),
-				y: Math.floor(evt.clientY - rect.top)
-			};
-		} else {
-			return {
-				x: evt.clientX - rect.left,
-				y: evt.clientY - rect.top
-			};
-		}
-	}*/
-
 };
