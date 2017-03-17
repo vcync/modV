7c633866b2273149d7db74307415727af32c314f
Sam Wray <sam@wray.pro>
2017-03-17 17:15:21 +0000

    Switch to existing shader compiler lib

--- src/shader-env/make-program.js
+++ src/shader-env/make-program.js
@@ -1,21 +1,35 @@
+var compiler = require('webgl-compile-shader');
+
 module.exports = (gl) => {
 	return function makeProgram(vertexSource, fragmentSource) {
 
-		let vertexShader = gl.createShader(gl.VERTEX_SHADER);
-		let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-		let program = gl.createProgram();
+		// let vertexShader = gl.createShader(gl.VERTEX_SHADER);
+		// let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+		// let program = gl.createProgram();
 
-		gl.shaderSource(vertexShader, vertexSource);
-		gl.compileShader(vertexShader);
-		
-		gl.shaderSource(fragmentShader, fragmentSource);
-		gl.compileShader(fragmentShader);
+		// gl.shaderSource(vertexShader, vertexSource);
+		// gl.compileShader(vertexShader);
 
-		gl.attachShader(program, vertexShader);
-		gl.attachShader(program, fragmentShader);
+		// gl.shaderSource(fragmentShader, fragmentSource);
+		// gl.compileShader(fragmentShader);
 
-		gl.linkProgram(program);
+		// gl.attachShader(program, vertexShader);
+		// gl.attachShader(program, fragmentShader);
 
-		return program;
+		// gl.linkProgram(program);
+
+		// return program;
+
+		var info = compiler({
+			vertex: vertexSource,
+			fragment: fragmentSource,
+
+			//optional args
+			gl: gl, //WebGL context; if not specified a new one will be created
+			verbose: true, //whether to emit console.warn messages when throwing errors
+			//attributeLocations: { ... key:index pairs ... },
+		});
+
+		return info.program;
 	};
 };
