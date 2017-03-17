58a914a65693db5f28acb6b3e67044a8aec16469
Sam Wray <sam@wray.pro>
2017-03-17 21:46:34 +0000

    Switch to existing shader compilation lib

--- src/shader-env/make-program-promise.js
+++ src/shader-env/make-program-promise.js
@@ -1,51 +1,26 @@
+var compiler = require('webgl-compile-shader');
+
 module.exports = (gl) => {
 	return function makeProgram(vertexSource, fragmentSource) {
 
 		return new Promise((resolve, reject) => {
+			var info;
 
-			let vertexShader = gl.createShader(gl.VERTEX_SHADER);
-			let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-			let program = gl.createProgram();
+			try {
+				info = compiler({
+					vertex: vertexSource,
+					fragment: fragmentSource,
 
-			gl.shaderSource(vertexShader, vertexSource);
-			gl.compileShader(vertexShader);
-			
-			let vsCompiled = gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS);
-
-			if(!vsCompiled) {
-				let vsCompilationLog = gl.getShaderInfoLog(vertexShader);
-				reject({
-					reason: 'Vertex shader did not compile',
-					log: vsCompilationLog
+					//optional args
+					gl: gl, //WebGL context; if not specified a new one will be created
+					verbose: true, //whether to emit console.warn messages when throwing errors
+					//attributeLocations: { ... key:index pairs ... },
 				});
+			} catch(e) {
+				reject(e);
 			}
 
-			gl.shaderSource(fragmentShader, fragmentSource);
-			gl.compileShader(fragmentShader);
-
-			let fsCompiled = gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS);
-
-			if(!fsCompiled) {
-				let fsCompilationLog = gl.getShaderInfoLog(fragmentShader);
-				reject({
-					reason: 'Fragment shader did not compile',
-					log: fsCompilationLog
-				});
-			}
-
-			gl.attachShader(program, vertexShader);
-			gl.attachShader(program, fragmentShader);
-
-			gl.linkProgram(program);
-
-			if(gl.getProgramInfoLog(program) !== '') {
-				reject({
-					reason: 'Program not valid',
-					log: gl.getProgramInfoLog(program)
-				});
-			}
-
-			resolve(program);
+			resolve(info.program);
 		});
 	};
 };
