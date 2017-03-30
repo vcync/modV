ce5fc544b648feea4cecd391ddb0be58d1eb0c1d
Sam Wray <sam@wray.pro>
2017-03-30 22:27:54 +0100

    Add a useProgram call to avoid warnings, remove old uniform code

--- src/shader-env/render.js
+++ src/shader-env/render.js
@@ -4,38 +4,30 @@ module.exports = (gl, env) => {
 	let programs = env.programs;
 	let setRectangle = env.setRectangle;
 
-	return function render(delta, canvas, pixelRatio) {
+	return function render(delta, canvas, pixelRatio, Module) {
 		// Clear WebGL canvas
 		gl.clearColor(0.0, 0.0, 0.0, 0.0);
 		gl.clear(gl.COLOR_BUFFER_BIT);
-		let programInfo = twgl.createProgramInfoFromProgram(gl, programs[env.activeProgram]);
-
-		// Set delta
-		// var deltaLocation = gl.getUniformLocation(programs[env.activeProgram], "u_delta");
-		// gl.uniform1f(deltaLocation, delta);
-
-		// // TODO: setup u_time & other usual uniforms
-		// var timeLocation = gl.getUniformLocation(programs[env.activeProgram], "u_time");
-		// gl.uniform1f(timeLocation, delta);
-
-		// var timeSecondsLocation = gl.getUniformLocation(programs[env.activeProgram], "iGlobalTime");
-		// gl.uniform1f(timeSecondsLocation, delta / 1000);
-
-		// var resolutionLocation = gl.getUniformLocation(programs[env.activeProgram], "iResolution");
-		// gl.uniform3f(resolutionLocation, canvas.width, canvas.height, 1.0);
+		let programInfo = Module.programInfo;
 
 		let uniforms = {
 			iGlobalTime: delta / 1000,
 			iDelta: delta,
 			u_delta: delta,
 			u_time: delta,
-			iResolution: [canvas.width, canvas.height, 1.0]
+			iResolution: [canvas.width, canvas.height, pixelRatio || 1.0]
 		};
 
+		for(var [key, value] of Module.uniformValues.entries()) {
+			uniforms[key] = value;
+		}
+
+		gl.useProgram(programInfo.program);
 		twgl.setUniforms(programInfo, uniforms);
 
 		// required as we need to resize our drawing boundaries for gallery and main canvases
-		// -- potential performance hinderance, consider seperate GL environment for gallery
+		// TODO: this is a performance hinderance, the most expensive call within this function,
+		// consider seperate GL environment for gallery
 		setRectangle(0, 0, canvas.width, canvas.height, env.buffer);
 
 		gl.drawArrays(gl.TRIANGLES, 0, 6);
