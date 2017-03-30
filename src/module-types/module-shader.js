2f207aab631189be0dfaaedb35b1b285f87b103d
Sam Wray <sam@wray.pro>
2017-03-30 22:29:39 +0100

    Add uniform and program build functions

--- src/module-types/module-shader.js
+++ src/module-types/module-shader.js
@@ -1,5 +1,6 @@
 const Module = require('./module');
 const makeProgramInit = require('../shader-env/make-program-promise');
+const twgl = require('twgl.js');
 
 module.exports = function(modV) {
 	modV.prototype.ModuleShader = class ModuleShader extends Module {
@@ -8,28 +9,8 @@ module.exports = function(modV) {
 			super(settings);
 
 			//if(!('shaderFile' in settings)) throw new this.ModuleError('Module had no path to shader in settings.shaderFile');
-
-			// Settings passed, expose shaderFile
-			this.shaderFile = settings.shaderFile;
-
-			// Loop through Uniforms, expose uniforms and create local variables
-			if('uniforms' in settings.info) {
-				forIn(settings.info.uniforms, (uniformKey, uniform) => {
-					switch(uniform.type) {
-						case 'f':
-							this[uniformKey] = parseFloat(uniform.value);
-							break;
-
-						case 'i':
-							this[uniformKey] = parseInt(uniform.value);
-							break;
-
-						case 'b':
-							this[uniformKey] = uniform.value;
-							break;
-					}
-				});
-			}
+			this.programIndex = -1;
+			this.uniformValues = new Map();
 		}
 
 		_makeProgram(gl, modV) {
@@ -82,6 +63,8 @@ module.exports = function(modV) {
 								gl.useProgram(program);
 								this.programIndex = modV.shaderEnv.programs.push(program)-1;
 
+								this.programInfo = twgl.createProgramInfoFromProgram(gl, program);
+
 								// finish up
 								resolve(this, 'shader');
 							}).catch(error => {
@@ -98,5 +81,57 @@ module.exports = function(modV) {
 				);
 			});
 		}
+
+		_makeProgramInfoFromIndex(modV) {
+			let program = modV.shaderEnv.programs[this.programIndex];
+			let gl = modV.shaderEnv.gl;
+			this.programInfo = twgl.createProgramInfoFromProgram(gl, program);
+		}
+
+		_setupUniforms(gl) {
+			const settings = this.settings;
+			let programInfo = this.programInfo;
+			let twGLUniforms = programInfo.uniformSetters;
+
+			forIn(settings.info.uniforms, (uniformKey, uniform) => {
+				if(uniformKey in twGLUniforms) {
+					let uniformSetter = twGLUniforms[uniformKey];
+					Object.defineProperty(this, uniformKey, {
+						set: (value) => {
+							this.uniformValues.set(uniformKey, value);
+						},
+						get: () => {
+							this.uniformValues.get(uniformKey);
+						}
+					});
+
+					gl.useProgram(this.programInfo.program);
+					uniformSetter(uniform.value);
+					this.uniformValues.set(uniformKey, uniform.value);
+				}
+			});
+		}
 	};
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
