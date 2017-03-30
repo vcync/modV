afe5e760020006449c16c8e8b2497a776aed1059
Sam Wray <sam@wray.pro>
2017-03-30 22:37:45 +0100

    Update for 1.5 Control Classes

--- src/modV-robot.js
+++ src/modV-robot.js
@@ -15,14 +15,13 @@ let modVBot = function(module, controlKey, modV) {
 	let interval = bpmToMs(modV.bpm);
 
 	let control = module.info.controls[controlKey];
-	let controlNode = document.getElementById(control.getID());
 
 	function loop() {
 		//for(let cntrl in module.info.controls) {
 
 			if(control instanceof modV.CheckboxControl) {
 
-				controlNode.checked = module[control.variable] = !Math.round(Math.random());
+				control.update(!Math.round(Math.random()));
 
 
 			} else if(!(control.type instanceof modV.VideoControl) && !(control.type instanceof modV.ImageControl)) {
@@ -32,38 +31,17 @@ let modVBot = function(module, controlKey, modV) {
 				switch(control.varType) {
 					case 'int':
 						rand = randomIntFromRange(control.min, control.max);
-
-						if('append' in control) {
-							controlNode.value = rand;
-							module[control.variable] = rand + control.append;
-						} else {
-							controlNode.value = module[control.variable] = rand;
-						}
-
+						control.update(rand);
 						break;
 
 					case 'float':
 						rand = randomFloatFromRange(control.min, control.max);
-
-						if('append' in control) {
-							controlNode.value = rand;
-							module[control.variable] = rand + control.append;
-						} else {
-							controlNode.value = module[control.variable] = rand;
-						}
-
+						control.update(rand);
 						break;
 
 					default:
 						rand = String.fromCharCode(0x30A0 + Math.random() * (0x30FF-0xFFFF+1));
-
-						if('append' in control) {
-							controlNode.value = rand;
-							module[control.variable] = rand + control.append;
-						} else {
-							controlNode.value = module[control.variable] = rand;
-						}
-
+						control.update(rand);
 						break;
 				}
 
@@ -93,7 +71,7 @@ module.exports = function(modV) {
 	modV.prototype.attachBot = function(module, controlKey) {
 		let mod = this.activeModules[module];
 		let control = mod.info.controls[controlKey];
-		let controlID = control.getID();
+		let controlID = control.id;
 
 		if(this.bots[controlID]) return false; // Bot already attached
 		let bot = new modVBot(mod, controlKey, this); //jshint ignore: line
@@ -103,7 +81,7 @@ module.exports = function(modV) {
 	modV.prototype.removeBot = function(module, controlKey) {
 		let mod = this.activeModules[module];
 		let control = mod.info.controls[controlKey];
-		let controlID = control.getID();
+		let controlID = control.id;
 
 		if(!this.bots[controlID]) return false; // No bot
 		this.bots[controlID].removeBot();
