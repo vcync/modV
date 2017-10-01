c98145f7ed6f58ab2656518a5b55387c9e34d44c
Sam Wray <sam@wray.pro>
2017-10-01 20:12:00 +0100

    Return promises for hot async action

--- store/modules/modv-modules.js
+++ store/modules/modv-modules.js
@@ -62,56 +62,58 @@ const getters = {
 
 // actions
 const actions = {
-  createActiveModule({ commit, state }, { moduleName, appendToName, skipInit }) {
-    const module = new state.registry[moduleName]();
-    let newModuleName = generateName(module.info.name);
-    module.info.name = newModuleName;
-    module.info.alpha = 1;
-    module.info.originalName = moduleName;
-    module.info.enabled = true;
-    module.info.compositeOperation = 'normal';
+  createActiveModule({ commit, state }, { moduleName, appendToName, skipInit, enabled }) {
+    return new Promise((resolve, reject) => {
+      const module = new state.registry[moduleName]();
+      let newModuleName = generateName(module.info.name);
+      module.info.name = newModuleName;
+      module.info.alpha = 1;
+      module.info.originalName = moduleName;
+      module.info.enabled = enabled || false;
+      module.info.compositeOperation = 'normal';
 
-    const dimensions = store.getters['size/dimensions'];
-    const useDpr = store.getters['user/useRetina'];
+      const dimensions = store.getters['size/dimensions'];
+      const useDpr = store.getters['user/useRetina'];
 
-    if(useDpr) {
-      dimensions.width *= window.devicePixelRatio;
-      dimensions.height *= window.devicePixelRatio;
-    }
-
-    const canvas = modV.bufferCanvas;
-
-    if('init' in module && !skipInit) module.init(canvas);
-
-    if('meyda' in module.info) {
-      if(Array.isArray(module.info.meyda)) {
-        module.info.meyda.forEach(feature =>
-          store.commit('meyda/addFeature', { feature })
-        );
+      if(useDpr) {
+        dimensions.width *= window.devicePixelRatio;
+        dimensions.height *= window.devicePixelRatio;
       }
-    }
 
-    newModuleName = `${newModuleName}${appendToName || ''}`;
+      const canvas = modV.bufferCanvas;
 
-    if('controls' in module.info) {
-      Object.keys(module.info.controls).forEach((key) => {
-        const control = module.info.controls[key];
-        const inputId = `${newModuleName}-${control.variable}`;
+      if('init' in module && !skipInit) module.init(canvas);
 
-        if(control.type === 'paletteControl') {
-          store.dispatch('palettes/createPalette', {
-            id: inputId,
-            colors: control.colors || [],
-            duration: control.timePeriod,
-            moduleName: newModuleName,
-            variable: control.variable
-          });
+      if('meyda' in module.info) {
+        if(Array.isArray(module.info.meyda)) {
+          module.info.meyda.forEach(feature =>
+            store.commit('meyda/addFeature', { feature })
+          );
         }
-      });
-    }
+      }
 
-    commit('addActiveModule', { module, moduleName: newModuleName });
-    return module;
+      newModuleName = `${newModuleName}${appendToName || ''}`;
+
+      if('controls' in module.info) {
+        Object.keys(module.info.controls).forEach((key) => {
+          const control = module.info.controls[key];
+          const inputId = `${newModuleName}-${control.variable}`;
+
+          if(control.type === 'paletteControl') {
+            store.dispatch('palettes/createPalette', {
+              id: inputId,
+              colors: control.colors || [],
+              duration: control.timePeriod,
+              moduleName: newModuleName,
+              variable: control.variable
+            });
+          }
+        });
+      }
+
+      commit('addActiveModule', { module, moduleName: newModuleName });
+      resolve(module);
+    });
   },
   removeActiveModule({ commit }, { moduleName }) {
     const Module = externalState.active[moduleName];
