7cfc08fff33366f28b0357c4504421d5a2b2f1cd
Sam Wray <sam@wray.pro>
2017-11-20 17:36:13 +0000

    Adds pinned control panels

---
+++ src/components/ControlPanelHandler.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="column is-12 columns is-gapless is-mobile">
+    <control-panel
+      v-for="moduleName in panels"
+      :moduleName="moduleName"
+      :pinned="isPinned(moduleName)"
+      :focused="isFocused(moduleName)"
+    />
+  </div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex';
+  import ControlPanel from '@/components/ControlPanel';
+
+  export default {
+    name: 'controlPanelHandler',
+    data() {
+      return {
+
+      };
+    },
+    computed: {
+      ...mapGetters('modVModules', [
+        'focusedModule',
+      ]),
+      ...mapGetters('controlPanels', [
+        'pinnedPanels',
+      ]),
+      focusedModuleName() {
+        if (!this.focusedModule) return '';
+        return this.focusedModule.info.name;
+      },
+      panels() {
+        const panels = [].concat(this.pinnedPanels);
+
+        if (this.focusedModule && !this.isPinned(this.focusedModuleName)) {
+          panels.push(this.focusedModuleName);
+        }
+
+        return panels;
+      },
+    },
+    methods: {
+      ...mapGetters('modVModules', [
+        'getActiveModule',
+      ]),
+      isPinned(moduleName) {
+        return this.pinnedPanels.indexOf(moduleName) > -1;
+      },
+      isFocused(moduleName) {
+        return moduleName === this.focusedModuleName;
+      },
+    },
+    components: {
+      ControlPanel,
+    },
+  };
+</script>
+
+<style scoped>
+
+</style>
---
+++ store/modules/control-panels.js
@@ -0,0 +1,31 @@
+const state = {
+  pinnedPanels: [],
+};
+
+// getters
+const getters = {
+  pinnedPanels: state => state.pinnedPanels,
+};
+
+// actions
+const actions = {
+
+};
+
+// mutations
+const mutations = {
+  pinPanel(state, { moduleName }) {
+    state.pinnedPanels.push(moduleName);
+  },
+  unpinPanel(state, { moduleName }) {
+    state.pinnedPanels.splice(state.pinnedPanels.indexOf(moduleName), 1);
+  },
+};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations,
+};
