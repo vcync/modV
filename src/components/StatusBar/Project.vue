a59547eb3cf31597c1b14cd3bc05f588c2ea12f4
Sam Wray <sam@wray.pro>
2018-08-06 18:30:17 +0100

    feat(project): Add project status bar display and quick switcher

---
+++ src/components/StatusBar/Project.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <span class="tag" @dblclick="modalOpen = true">{{ $store.state.projects.currentProject }}</span>
+
+    <b-modal :active.sync="modalOpen">
+      <div class="card">
+        <div class="card-content">
+          <h1 class="title">Projects</h1>
+          <div class="columns is-mobile is-multiline">
+            <div
+              class="column is-12"
+              v-for="projectName in Object.keys($store.state.projects.projects)"
+            >
+
+              <div class="columns">
+                <div class="column is-10">
+                  {{ projectName }}
+                </div>
+                <div class="column is-2">
+                  <button
+                    class="button is-dark is-outlined"
+                    :class="{ 'is-loading': loading === projectName }"
+                    :disabled="!isCurrent(projectName)"
+                    @click="useProject({ projectName })"
+                  >{{ isCurrent(projectName) ? 'Use' : 'In use' }}</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'statusBarItemProject',
+  data() {
+    return {
+      modalOpen: false,
+    };
+  },
+  methods: {
+    useProject({ projectName }) {
+      this.$store.dispatch('projects/setCurrent', { projectName });
+    },
+    isCurrent(projectName) {
+      return this.$store.state.projects.currentProject !== projectName;
+    },
+  },
+};
+</script>
