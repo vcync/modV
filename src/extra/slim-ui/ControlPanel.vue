63dc7fa5597caf9a59d55be1cc21f62ec41bb7f8
Tim Pietrusky <timpietrusky@gmail.com>
2018-08-06 14:59:20 +0200

    feat(slimui): Minimal UI for modV
    A plugin that provides a reduced style for the module list.
    
    Fixes #177

---
+++ src/extra/slim-ui/ControlPanel.vue
@@ -0,0 +1,63 @@
+<template>
+</template>
+
+<script>
+  /**
+   * The styles defined here are there to overwrite styles in the main application.
+   * This is not the best idea, but something that can be done without hacking anything
+   * into modV. A better way would be to add the styles directly in index.js. 
+   */
+  export default {
+    name: 'slimUiControlPanel',
+    methods: {
+    },
+  };
+</script>
+
+<style lang="scss">
+  .slimui {
+    // In the list of modules:
+    .module-list {
+      
+      .options {
+        // Remove opacity & blending
+        .opacity-group,
+        .blending-group {
+          display: none;
+        }
+
+        // Remove the "enabled" label
+        .enable-group {
+          label {
+            display: none;
+          }
+          label.checkbox {
+            display: inline-flex;
+          }
+        }
+      }
+
+      // For every module in a layer:
+      .active-item {
+
+        // Decrease the size of the module title
+        .active-item-title {
+          font-size: 1em;
+          float: left;
+          width: 100%;
+        }
+
+        // Decrease the size of the module container
+        .active-module-wrapper {
+          height: 40px;
+        }
+
+        // Decrease the size of the reorder icon
+        .fa-reorder {
+          font-size: 1em;
+        }
+      }
+    }
+
+  }
+</style>
