744889d67b2606ff0f68df5236d7c6d31310f9d2
Tim Pietrusky <timpietrusky@gmail.com>
2018-08-07 00:34:38 +0200

    chore(clean): Removed "old" folder
    Fixes #220

--- old/src/main-window-resize.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(modV) {
-	modV.prototype.mainWindowResize = function() {
-		let boundingRect = this.previewCanvas.getBoundingClientRect();
-		this.previewCanvas.width = boundingRect.width;
-		this.previewCanvas.height = boundingRect.height;
-
-		this.calculatePreviewCanvasValues();
-	};
-};
--- old/src/media-manager/save-option.js
+++
@@ -1,7 +0,0 @@
-module.exports = function saveOption(option, value) {
-	this.sendJSON({
-		request: 'save-option',
-		key: option,
-		value: value
-	});
-};
