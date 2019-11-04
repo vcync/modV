ce02df13e29cc2d9c15876a7dc7fef307f8d68e8
Sam Wray <sam@wray.pro>
2019-11-04 21:12:08 +0000

    feat(bpmdisplay): adds BPM source information on hover

--- src/components/StatusBar/BPMDisplay.vue
+++ src/components/StatusBar/BPMDisplay.vue
@@ -1,3 +1,16 @@
 <template>
-  <div>{{ Math.round($modV.store.state.beats.bpm) }} BPM</div>
+  <div>
+    {{ Math.round($modV.store.state.beats.bpm) }} BPM
+    <span>({{ $modV.store.state.beats.bpmSource }})</span>
+  </div>
 </template>
+
+<style scoped>
+span {
+  display: none;
+}
+
+div:hover span {
+  display: inline;
+}
+</style>
