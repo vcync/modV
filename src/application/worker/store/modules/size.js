31c40201a8bb2d83ef135f5732a9e76fc057f0a4
Sam Wray <sam@wray.pro>
2019-11-04 21:13:34 +0000

    fix(size): fixes resizing not applying to active modules

--- src/application/worker/store/modules/size.js
+++ src/application/worker/store/modules/size.js
@@ -13,6 +13,17 @@ const actions = {
   async setSize({ commit }, { width, height }) {
     await store.dispatch("outputs/resize", { width, height });
 
+    const modulesKeys = Object.keys(store.state.modules.active);
+    const modulesLength = Object.keys(modulesKeys).length;
+    for (let i = 0; i < modulesLength; ++i) {
+      const module = store.state.modules.active[modulesKeys[i]];
+      if (module.resize && !module.isGallery) {
+        module.resize({
+          canvas: store.state.outputs.main.canvas
+        });
+      }
+    }
+
     commit("SET_SIZE", { width, height });
   }
 };
