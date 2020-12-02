5dc302104252ac1f5338132ddb3d3407647dd810
Tim Pietrusky <timpietrusky@gmail.com>
2020-12-02 21:27:35 +0100

    Next (#472)
    * feat(texture): Class to render a 2D texture (#467)
    
    * feat(texture): Class to render a 2D texture
    
    In order to load different kind of textures (e.g. another layer or an image) into modV and scale it
    or change it position, this class can be used.
    
    fix #329
    
    * feat(texture): Set the offset using percentage values
    
    * feat(texture): Set min / max to -100 to 100
    
    * refactor(bar): Set strobe default to 0 (#471)
    
    fix #452

---
+++ src/application/sample-modules/Texture2d.js
@@ -0,0 +1,56 @@
+export default {
+  meta: {
+    name: "Texture 2D",
+    type: "2d",
+    version: "1.0.0",
+    author: "NERDDISCO"
+  },
+  props: {
+    texture: {
+      type: "texture"
+    },
+    scale: {
+      label: "Scale",
+      type: "float",
+      default: 1,
+      min: 0,
+      max: 5,
+      step: 0.001
+    },
+    offsetX: {
+      label: "Offset X in %",
+      type: "float",
+      default: 0,
+      min: -100,
+      max: 100,
+      step: 1
+    },
+    offsetY: {
+      label: "Offset Y in %",
+      type: "float",
+      default: 0,
+      min: -100,
+      max: 100,
+      step: 1
+    }
+  },
+  draw({ canvas: { width, height }, context, props }) {
+    const { scale, offsetX, offsetY } = props;
+
+    if (props.texture.value) {
+      const { width: imageWidth, height: imageHeight } = props.texture.value;
+      const x = (width - imageWidth * scale) / 2;
+      const y = (height - imageHeight * scale) / 2;
+      const calculatedOffsetX = (width / 100) * offsetX;
+      const calculatedOffsetY = (height / 100) * offsetY;
+
+      context.drawImage(
+        props.texture.value,
+        x + calculatedOffsetX,
+        y + calculatedOffsetY,
+        imageWidth * scale,
+        imageHeight * scale
+      );
+    }
+  }
+};
