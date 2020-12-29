548c927124b12dbbc252b41a7c8c6426234360ef
Tim Pietrusky <timpietrusky@gmail.com>
2020-12-29 21:31:45 +0100

    feat(midi): Support for NoteOn and CC buttons (#507)
    * feat(midi): Support for NoteOn and CC buttons
    
    fix #506
    
    * feat(midi): Renamed ccAsNoteOn into ccLatch and added noteOnLatch
    
    * chore(midi): Remove unused console.log
    
    * fix(midi): changes spread operator to foreach loop to replace state
    
    The spread was causing the store module's state to lose the Vuex Observer, this forEach loop retains
    the Observer.
    
    fix #523
    
    Co-authored-by: Sam Wray <sam@wray.pro>

--- src/components/InputDeviceConfig/MIDI.vue
+++ src/components/InputDeviceConfig/MIDI.vue
@@ -11,14 +11,16 @@
   >
     <c span="1">MIDI Inputs</c>
     <c span="1..">
-      <grid columns="4">
+      <grid columns="5">
         <c>Name</c>
         <c>Input</c>
         <c>Clock</c>
+        <c>CC Latch</c>
+        <c>NoteOn Latch</c>
       </grid>
     </c>
     <c span="1.." v-for="(device, deviceId) in devices" :key="deviceId">
-      <grid columns="4">
+      <grid columns="5">
         <c>{{ device.name }}</c>
         <c>
           <Button
@@ -34,6 +36,22 @@
             @click="toggleClock(deviceId, !device.listenForClock)"
           >
             {{ device.listenForClock ? "On" : "Off" }}
+          </Button>
+        </c>
+        <c>
+          <Button
+            class="light"
+            @click="toggleCcLatch(deviceId, !device.ccLatch)"
+          >
+            {{ device.ccLatch ? "On" : "Off" }}
+          </Button>
+        </c>
+        <c>
+          <Button
+            class="light"
+            @click="toggleNoteOnLatch(deviceId, !device.noteOnLatch)"
+          >
+            {{ device.noteOnLatch ? "On" : "Off" }}
           </Button>
         </c>
       </grid>
@@ -70,6 +88,22 @@ export default {
       this.$modV.store.commit("midi/UPDATE_DEVICE", {
         id,
         key: "listenForClock",
+        value
+      });
+    },
+
+    toggleCcLatch(id, value) {
+      this.$modV.store.commit("midi/UPDATE_DEVICE", {
+        id,
+        key: "ccLatch",
+        value
+      });
+    },
+
+    toggleNoteOnLatch(id, value) {
+      this.$modV.store.commit("midi/UPDATE_DEVICE", {
+        id,
+        key: "noteOnLatch",
         value
       });
     }
