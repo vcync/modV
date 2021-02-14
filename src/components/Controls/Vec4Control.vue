f4d20d4a425197e8ac7cf52acf7e6f71b6921e4c
Sam Wray <sam@wray.pro>
2021-02-14 00:10:38 +0000

    feat(vec3/4): adds controls for vec3 and vec4
    Implements controls for vec3 and vec4 using existing color picker components
    
    fixes #372. fixes #373
    
    (cherry picked from commit 1fd34f081fe8beeee1c8ccdee8fefb2c9c1679a9)

---
+++ src/components/Controls/Vec4Control.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <Sketch :value="color" @input="updateValue" ref="picker" />
+  </div>
+</template>
+
+<script>
+import { Sketch } from "vue-color";
+
+export default {
+  props: {
+    value: {
+      default: () => [],
+      required: true
+    }
+  },
+
+  components: {
+    Sketch
+  },
+
+  data() {
+    return {
+      color: { r: 0, g: 0, b: 0, a: 1 }
+    };
+  },
+
+  created() {
+    this.color = this.vec4ToRgba(this.value);
+  },
+
+  watch: {
+    value(vec4) {
+      this.color = this.vec4ToRgba(vec4);
+    }
+  },
+
+  methods: {
+    updateValue(color) {
+      this.color = color.rgba;
+      this.$emit("input", this.rgbaToVec4(this.color));
+    },
+
+    rgbaToVec4({ r, g, b, a }) {
+      return [r / 255, g / 255, b / 255, a];
+    },
+
+    vec4ToRgba([r, g, b, a]) {
+      return { r: r * 255, g: g * 255, b: b * 255, a };
+    }
+  }
+};
+</script>
