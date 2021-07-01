1894edabfb44ff7cf700c5166c1719b0c0eec606
Sam Wray <sam@wray.pro>
2021-07-01 21:44:59 +0100

    feat(group-inheritance): adds group inheritance option (#607)
    restores modV 2 functionality. replaces the inheritance checkbox on groups with a select input
    
    fixes #603
    
    Co-authored-by: Tim Pietrusky <timpietrusky@gmail.com>

--- src/application/worker/store/modules/groups.js
+++ src/application/worker/store/modules/groups.js
@@ -22,9 +22,9 @@ import uuidv4 from "uuid/v4";
  * @property {Boolean} inherit             Indicates whether the Group should inherit from another
  *                                         Group at redraw
  *
- * @property {Number}  inheritFrom         The target Group to inherit from, -1 being the previous
- *                                         Group in modV#Groups, 0-n being the index of
- *                                         another Group within modV#Groups
+ * @property {String|Number}  inheritFrom  The target Group to inherit from, -1 being the previous
+ *                                         Group in modV#Groups, UUID4 string being the ID of
+ *                                         another Group within modV.store.state.groups.groups
  *
  * @property {Boolean} pipeline            Indicates whether the Group should render using pipeline
  *                                         at redraw
@@ -120,6 +120,7 @@ const actions = {
       hidden: args.hidden || false,
       modules: args.modules || [],
       inherit,
+      inheritFrom: -1,
       alpha: args.alpha || 1,
       compositeOperation: args.compositeOperation || "normal",
       context: await store.dispatch("outputs/getAuxillaryOutput", {
