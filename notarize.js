b4944d97a9b5ffabf85949c291a7ee847851f9ec
Sam Wray <sam@wray.pro>
2022-11-05 01:03:48 +0000

    fix(build-macos): add team id (#778)

--- notarize.js
+++ notarize.js
@@ -13,12 +13,14 @@ exports.default = async function notarizing(context) {
 
   const appName = context.packager.appInfo.productFilename;
 
-  console.log("Notarizing macOS application…");
+  console.log("  • notarizing");
 
   return await notarize({
+    tool: "notarytool",
     appBundleId: "gl.vcync.modv",
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLEID,
-    appleIdPassword: process.env.APPLEIDPASS
+    appleIdPassword: process.env.APPLEIDPASS,
+    teamId: process.env.APPLE_TEAM_ID
   });
 };
