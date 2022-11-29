7ce6eaabe53f265fc1fd66ddea72699c278c3089
Sam Wray <sam@wray.pro>
2022-11-29 21:12:28 +0000

    fix: set value on checkbox

--- src/components/Plugins.vue
+++ src/components/Plugins.vue
@@ -24,6 +24,7 @@
                 <c span="3">
                   <Checkbox
                     @input="handleEnableInput(plugin.id)"
+                    :value="plugin.enabled"
                     :emitBoolean="true"
                     class="light"
                   />
