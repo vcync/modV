1e2ff7f2cbd4c226e34163a204712e0bc6ff32ac
Sam Wray <sam@wray.pro>
2022-12-21 20:55:36 +0000

    refactor(right-click-input): remove unused code

--- src/components/inputs/RightClickNumberInput.vue
+++ src/components/inputs/RightClickNumberInput.vue
@@ -79,24 +79,4 @@ export default {
   display: flex;
   align-items: center;
 }
-/* canvas {
-  border-radius: 23px;
-  width: 100%;
-}
-
-div {
-  position: relative;
-  width: 100%;
-  height: 16px;
-  display: inline-block;
-}
-
-input {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  width: 100%;
-} */
 </style>
