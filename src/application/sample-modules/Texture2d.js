99c7f10c7a9186387102c75ebd008fb2df42cfe9
Sam Wray <sam@wray.pro>
2023-01-07 14:03:39 +0000

    feat(module): add size constraint to texture2d

--- src/application/sample-modules/Texture2d.js
+++ src/application/sample-modules/Texture2d.js
@@ -32,24 +32,75 @@ export default {
       min: -100,
       max: 100,
       step: 1
+    },
+    constrain: {
+      label: "Constrain",
+      type: "enum",
+      default: "none",
+      enum: [
+        { label: "None (Scale)", value: "none" },
+        { label: "Contain", value: "contain" },
+        { label: "Cover", value: "cover" }
+      ]
     }
   },
   draw({ canvas: { width, height }, context, props }) {
-    const { scale, offsetX, offsetY } = props;
+    const { constrain, offsetX, offsetY, scale, texture } = props;
 
-    if (props.texture.value) {
-      const { width: imageWidth, height: imageHeight } = props.texture.value;
-      const x = (width - imageWidth * scale) / 2;
-      const y = (height - imageHeight * scale) / 2;
+    if (texture.value) {
+      let { width: imageWidth, height: imageHeight } = texture.value;
+
+      let x;
+      let y;
+
+      if (constrain === "contain") {
+        imageHeight = (imageHeight / imageWidth) * width;
+        imageWidth = width;
+
+        y = (height - imageHeight) / 2;
+        x = 0;
+
+        if (imageHeight > height) {
+          imageWidth = (imageWidth / imageHeight) * height;
+          imageHeight = height;
+
+          y = 0;
+          x = (width - imageWidth) / 2;
+        }
+      } else if (constrain === "cover") {
+        const imageRatio = imageHeight / imageWidth;
+        const canvasRatio = height / width;
+
+        if (imageRatio < canvasRatio) {
+          imageWidth = (width * canvasRatio) / imageRatio;
+          imageHeight = height;
+
+          x = (width - imageWidth) / 2;
+          y = 0;
+        } else {
+          imageHeight = width * imageRatio;
+          imageWidth = width;
+
+          x = 0;
+          y = (height - imageHeight * scale) / 2;
+        }
+      } else {
+        imageWidth = imageWidth * scale;
+        imageHeight = imageHeight * scale;
+
+        x = (width - imageWidth) / 2;
+        y = (height - imageHeight) / 2;
+      }
+
       const calculatedOffsetX = (width / 100) * offsetX;
       const calculatedOffsetY = (height / 100) * offsetY;
 
       context.drawImage(
-        props.texture.value,
+        texture.value,
         x + calculatedOffsetX,
         y + calculatedOffsetY,
-        imageWidth * scale,
-        imageHeight * scale
+        imageWidth,
+        imageHeight
       );
     }
   }
