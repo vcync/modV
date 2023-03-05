c5576ed192ff2974ea3ae7ad79eab8dbe8e86b42
Sam Wray <sam@wray.pro>
2023-03-05 00:34:52 +0000

    test: add inheritance test

--- tests/e2e/groups.spec.js
+++ tests/e2e/groups.spec.js
@@ -123,4 +123,42 @@ test.describe("groups", () => {
       );
     }
   });
+
+  test("inherit and inheritFrom state can be set", async () => {
+    const userGroups = modVApp.groups.getUserGroups();
+    const {
+      groupIndex,
+      groupId
+    } = await modVApp.groups.getFirstUserGroupIdAndIndex();
+
+    const { inheritSelect } = modVApp.groups.getControlLocators(groupId);
+
+    await modVApp.groups.showControls(groupId);
+
+    const values = [-2, -1];
+    for (let i = 0; i < userGroups.length; i += 1) {
+      values.push(i);
+    }
+
+    for (let i = 0; i < values.length; i += 1) {
+      const value = values[i];
+      await inheritSelect.selectOption(String(value));
+
+      await modVApp.page.waitForTimeout(100);
+
+      if (value === -2) {
+        await modVApp.checkWorkerAndMainState(state =>
+          expect(state.groups.groups[groupIndex].inherit).toBe(false)
+        );
+      } else {
+        await modVApp.checkWorkerAndMainState(state =>
+          expect(state.groups.groups[groupIndex].inherit).toBe(true)
+        );
+
+        await modVApp.checkWorkerAndMainState(state =>
+          expect(state.groups.groups[groupIndex].inheritFrom).toBe(value)
+        );
+      }
+    }
+  });
 });
